e55457084e44ac9cc3060f97b7e388ca22141e08
Gabriel Méndez <yo@gabrielmendezc.com>
2022-05-22 13:22:32 +0200

    deploy redis to prod

--- app/utils/redis.server.ts
+++ app/utils/redis.server.ts
@@ -1,8 +1,13 @@
 import Redis from "ioredis";
 
-const redis = new Redis({
-  host: process.env.REDIS_HOST,
-  password: process.env.REDIS_PASSWORD
-});
+const REDIS_URL = process.env.REDIS_URL;
+if (!REDIS_URL) {
+  throw new Error("REDIS_URL environment variable is missing");
+}
+
+const redis = new Redis(
+  REDIS_URL,
+  process.env.NODE_ENV === "development" ? {} : { family: 6 }
+);
 
 export { redis };
